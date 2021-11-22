7f86662e08cd9e295f30e17fa4fcbfc23fed2fd9
Enzo Calamia <hello@ecalamia.com>
2021-11-22 22:12:05 +0100

    Encapsulate server in a "Champignon"

--- server/server.go
+++ server/server.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/websocket"
 	"net/http"
 	"os"
 	"sync"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{}
@@ -14,6 +15,11 @@ var appSecret = []byte("this_is_secret")
 var creationDate = time.Date(2021, 11, 16, 17, 04, 22, 324243, time.UTC)
 var creationDateMs = creationDate.UnixNano() / 1e6 // Use UnixMilli() with Go 1.17
 var interval = 15000                               // msecs
+
+type Champignon struct {
+	Server *http.Server
+	Seed   []byte
+}
 
 type SafeConn struct {
 	mu sync.Mutex
@@ -27,18 +33,44 @@ func (sc *SafeConn) WriteJSON(v interface{}) error {
 	return err
 }
 
+func (champ *Champignon) Serve() {
+	champ.Server.ListenAndServe()
+}
+
 func main() {
 	if time.Now().Before(creationDate) {
 		fmt.Println("creationDate", creationDate)
 		fmt.Println("Now", time.Now())
 		panic("creationDate < Now")
 	}
-	port := os.Args[1]
-	http.HandleFunc("/play", play) // WS handler
-	http.ListenAndServe("[::1]:"+port, nil)
+	host := os.Args[1]
+	seed := os.Getenv("CHAMPI_SEED")
+	fmt.Println("Server seed:", seed)
+	server := &http.Server{
+		Addr:         host,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	champ, _ := NewChampignon(seed, server)
+	fmt.Printf("Champignon server started on %s\n", host)
+	champ.Serve()
 }
 
-func play(w http.ResponseWriter, r *http.Request) {
+func NewChampignon(seed string, server *http.Server) (*Champignon, error) {
+	wrapPlay := func(w http.ResponseWriter, r *http.Request) {
+		play([]byte(seed), w, r)
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/play", wrapPlay)
+	server.Handler = mux
+	champ := &Champignon{
+		server,
+		[]byte(seed),
+	}
+	return champ, nil
+}
+
+func play(seed []byte, w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	if len(q["room"]) == 0 || len(q["username"]) == 0 {
 		w.WriteHeader(http.StatusBadRequest)
@@ -60,7 +92,8 @@ func play(w http.ResponseWriter, r *http.Request) {
 	// goroutine that sends new questions when in due time
 	go func() {
 		fmt.Println("Room creation date:", creationDate)
-		roomSeed := append(appSecret, []byte(creationDate.String())...)
+		roomSeed := append(seed, []byte(creationDate.String())...)
+		fmt.Printf("Room seed: %s\from seed %s\n", roomSeed, seed)
 		for {
 			now := time.Now()
 			nowMs := now.UnixNano() / 1e6 // Use UnixMilli() with Go 1.17
--- server/server_test.go
+++ server/server_test.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"github.com/gorilla/websocket"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
+
+	"github.com/gorilla/websocket"
 )
 
 func TestJoinWithParams(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(play))
+	wrapPlay := func(w http.ResponseWriter, r *http.Request) {
+		play([]byte("seed_test"), w, r)
+	}
+	ts := httptest.NewServer(http.HandlerFunc(wrapPlay))
 	defer ts.Close()
 	wsURL := strings.Replace(ts.URL, "http://", "ws://", 1) + "?username=player&room=ROOM"
 	c, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
@@ -21,7 +26,10 @@ func TestJoinWithParams(t *testing.T) {
 }
 
 func TestJoinWithoutParams(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(play))
+	wrapPlay := func(w http.ResponseWriter, r *http.Request) {
+		play([]byte("seed_test"), w, r)
+	}
+	ts := httptest.NewServer(http.HandlerFunc(wrapPlay))
 	defer ts.Close()
 	wsURL := strings.Replace(ts.URL, "http://", "ws://", 1)
 	c, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
@@ -30,4 +38,18 @@ func TestJoinWithoutParams(t *testing.T) {
 		t.Errorf("Expected error here")
 		t.Log(c)
 	}
+}
+
+func TestNewChampignon(t *testing.T) {
+	seed := "seed_for_test"
+	server := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	defer server.Close()
+	champ, err := NewChampignon(seed, server)
+	go champ.Serve()
+	t.Log(champ)
+	t.Log(err)
 }
